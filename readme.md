f542f9b5c46f462ff6bd10625ed4550fe24b9d90
Igor Sechyn <igor@linear.app>
2022-06-16 12:29:05 +0300

    achore: initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+#Linear Airbyte Connector
--- src/streams/index.ts
+++
@@ -1,4 +0,0 @@
-import { Team } from "./team";
-import { Issue } from "./issue";
-import { Organization } from "./organization";
-export { Team, Issue, Organization };
