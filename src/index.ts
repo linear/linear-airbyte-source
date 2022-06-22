1ea8b3064f6a955e8198b2b1e591166ffeb97a64
Igor Sechyn <igor@linear.app>
2022-06-22 21:13:55 +0300

    afeat: added all streams

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,24 @@ import VError from "verror";
 import { LinearClient } from "./client/LinearClient";
 
 import { Issue } from "./streams";
+import { Attachment } from "./streams/attachment";
+import { AuditEntry } from "./streams/auditEntry";
+import { Comment } from "./streams/comment";
+import { Cycle } from "./streams/cycle";
+import { Document } from "./streams/document";
+import { IssueHistory } from "./streams/issueHistory";
+import { IssueLabel } from "./streams/issueLabel";
+import { IssueRelation } from "./streams/issueRelation";
+import { Milestone } from "./streams/milestone";
 import { Organization } from "./streams/organization";
+import { Project } from "./streams/project";
+import { ProjectLink } from "./streams/projectLink";
+import { ProjectUpdate } from "./streams/projectUpdate";
+import { Team } from "./streams/team";
+import { TeamKey } from "./streams/teamKey";
+import { TeamMembership } from "./streams/teamMemebership";
+import { User } from "./streams/user";
+import { WorkflowState } from "./streams/workflowState";
 
 /** The main entry point. */
 export function mainCommand(): Command {
@@ -46,6 +63,23 @@ class LinearSource extends AirbyteSourceBase {
     return [
       new Issue(this.logger, client),
       new Organization(this.logger, client),
+      new Team(this.logger, client),
+      new TeamKey(this.logger, client),
+      new TeamMembership(this.logger, client),
+      new User(this.logger, client),
+      new Milestone(this.logger, client),
+      new Project(this.logger, client),
+      new ProjectUpdate(this.logger, client),
+      new ProjectLink(this.logger, client),
+      new IssueHistory(this.logger, client),
+      new IssueLabel(this.logger, client),
+      new IssueRelation(this.logger, client),
+      new Attachment(this.logger, client),
+      new AuditEntry(this.logger, client),
+      new Comment(this.logger, client),
+      new Cycle(this.logger, client),
+      new WorkflowState(this.logger, client),
+      new Document(this.logger, client),
     ];
   }
 }
